d5791e8ca7cebdaa1c8ed39e9ed1009ca990f107
Olena Cherneha <166736208+olena4reunited@users.noreply.github.com>
2024-09-27 15:55:43 +0000

    fix: removed if in api_views and replace it by using raise_exeption attribute in is_valifg method of serializer

--- cinema/views.py
+++ cinema/views.py
@@ -14,10 +14,9 @@ def movie_list_create(request):
         return Response(serializer.data, status=status.HTTP_200_OK)
     else:
         serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exeption=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(["GET", "PUT", "DELETE"])
@@ -29,10 +28,9 @@ def movie_detail(request, pk):
         return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == "PUT":
         serializer = MovieSerializer(movie, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
     else:
         movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
